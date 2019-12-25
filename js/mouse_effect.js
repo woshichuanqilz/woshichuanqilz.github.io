6df69e3e67dfb5cdcf9c6948536edea6ad3363bf
OctoberNinth <513278236@qq.com>
2019-12-25 09:13:35 +0800

    add circle effect

--- js/mouse_effect.js
+++ js/mouse_effect.js
@@ -1,30 +1,19 @@
-// var H = 0;
-
-// // $('.button').bind('mousedown touchstart',function(e) {
-// //   $('.clicked').removeClass('clicked');
-// //   $(this).addClass('clicked');
-// // });
-
-// $(document).bind('mousemove touchmove',function(e) {
-    // e.preventDefault();
-    // var drawSize = 10;
-    // var drawType = '○';
-    // var floatType = 'floatOne';
-    // var xPos = e.originalEvent.pageX;
-    // var yPos = e.originalEvent.pageY;
+var H = 0;
+$(document).bind('mousemove touchmove',function(e) {
+    e.preventDefault();
+    var drawSize = 10;
+    var drawType = '○';
+    var floatType = 'floatOne';
+    var xPos = e.originalEvent.pageX;
+    var yPos = e.originalEvent.pageY;
     
-    // $('body').append('<div class="draw" style=" font-size:'+drawSize+'px;left:'+xPos+'px;top:'+yPos+'px;-webkit-animation:'+floatType+' .9s 1;-moz-animation:'+floatType+' .9s 1;color:#33a0fc;">'+drawType+'</div>');
+    $('body').append('<div class="draw" style=" font-size:'+drawSize+'px;left:'+xPos+'px;top:'+yPos+'px;-webkit-animation:'+floatType+' .9s 1;-moz-animation:'+floatType+' .9s 1;color:#33a0fc;">'+drawType+'</div>');
   
-    // $('.draw').each(function() {
-      // var div = $(this);
-      // setTimeout(function() {$(div).remove();},800);
-    // });
-// });
-
-// // setInterval(function() {
-// //   if(H<=360) {H++;}
-// //   else {H=0;}
-// // },10);
+    $('.draw').each(function() {
+      var div = $(this);
+      setTimeout(function() {$(div).remove();},800);
+    });
+});
 
 /**
  * Copyright (c) 2016 hustcc
---
+++ logfile.md
@@ -0,0 +1,4 @@
+## 赵子宵 Done
+
+![zzx](https://s2.ax1x.com/2019/12/25/liki0e.png)
+
